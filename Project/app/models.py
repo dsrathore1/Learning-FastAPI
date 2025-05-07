d84f7852aa67b9c5c90f136d7f58564840fdbbd9
PrayagRaj22 <prayagrajsingh55@protonmail.com>
2025-05-07 13:43:16 +0530

    Deleted some files and folders

--- Project/app/models.py
+++
@@ -1,9 +0,0 @@
-from sqlmodel import SQLModel, Field
-from typing import Optional
-
-
-#! Create Model
-class Todo(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    title: str
-    completed: bool = False
