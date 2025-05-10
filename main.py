fb31dd48fa6bfa5cfbf7b13ee6ad064436291630
dsrathore1 <dsrathorebsw1234@gmail.com>
2025-05-10 16:27:38 +0530

    Added mount and template file

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-form fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="Static"), name="Static")
+templates = Jinja2Templates(directory="Templates")
+
+
+@app.get("/")
+async def get_data():
+    return {
+        "message": "Hello, I am learning FastAPI",
+        "main-page": "http://127.0.0.1:8000/template",
+    }
+
+
+@app.get("/template", response_class=HTMLResponse)
+async def get_template(request: Request):
+    return templates.TemplateResponse(request=request, name="template.html")
