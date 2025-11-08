4cd3d4045f0a6124023e8c42bc964f3f69e55ff8
dsrathore1 <dsrathorebsw1234@gmail.com>
2025-11-08 19:18:23 +0530

    Deleted unwanted things

--- Routes/route_i.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter(prefix="/route1", tags=["Route 1"])
-
-
-@router.get("/")
-async def read_route1() -> dict[str, str]:
-    return {"message": "This is Route 1"}
--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 from fastapi import FastAPI
-from Routes import route_i, route_ii, route_iii, route_iv
 
 app = FastAPI()
 
-app.include_router(route_i.router)
-app.include_router(route_ii.router)
-app.include_router(route_iii.router)
-app.include_router(route_iv.router)
 
 
 @app.get("/")
